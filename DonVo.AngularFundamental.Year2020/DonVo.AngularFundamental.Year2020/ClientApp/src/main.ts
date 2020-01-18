542f03187a5fb3048057c9b4ecccf9d2c3ff017c
Don Vo - NC - USA <59782883+DonVo2020@users.noreply.github.com>
2020-01-18 10:10:18 -0500

    change one line

--- DonVo.AngularFundamental.Year2020/DonVo.AngularFundamental.Year2020/ClientApp/src/main.ts
+++ DonVo.AngularFundamental.Year2020/DonVo.AngularFundamental.Year2020/ClientApp/src/main.ts
@@ -4,6 +4,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+platformBrowserDynamic().bootstrapModule(AppModule);
+
 export function getBaseUrl() {
   return document.getElementsByTagName('base')[0].href;
 }
