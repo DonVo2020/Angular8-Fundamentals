f3373f78b9bdc2929646575c3d6178d0e591518b
Don Vo - NC - USA <59782883+DonVo2020@users.noreply.github.com>
2020-01-19 19:58:20 -0500

    Unused

--- DonVo.AngularFundamental.Year2020/DonVo.AngularFundamental.Year2020/ClientApp/src/app/031-lazy-loading/lazy-loading.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-lazy-loading',
-  templateUrl: './lazy-loading.component.html'
-})
-export class LazyLoadingComponent {
-}
